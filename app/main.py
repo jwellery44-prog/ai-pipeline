a461f5771dff02e01d0354d51558d9a311eeea28
jwellery44-prog <jwellery44@gmail.com>
2026-04-01 21:59:14 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -53,6 +53,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://ai-pipeline-frontend.vercel.app",
+        "https://jewel-india-frontend-uiet.vercel.app",
         "https://jwellery.arpitray.in",
     ],
     allow_credentials=True,
